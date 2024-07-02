857da30a8decad5aa1c348ab82140a113fc9c7ae
Sagnik Mukherjee <mukherjee.sagnik17@gmail.com>
2024-07-02 20:18:28 +0530

    recipeScreen

--- app/src/main/java/com/example/recepieapp/RecipeScreen.kt
+++ app/src/main/java/com/example/recepieapp/RecipeScreen.kt
@@ -1,9 +1,26 @@
 package com.example.recepieapp
-
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
-import java.lang.reflect.Modifier
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
 
 @Composable
-fun RecipeScreen(){
+fun RecipeScreen(modifier: Modifier){
+    val recipeViewModel: MainViewModel = viewModel()
+    val viewState by recipeViewModel.categoryState
+    Box(){
+        when{
+            viewState.loading->{
+                CircularProgressIndicator(modifier.align(Alignment.Center))
+            }
+            viewState.error != null ->{
 
+            }
+        }
+    }
 }
+
